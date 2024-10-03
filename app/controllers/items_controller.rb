16a6ebf1010e8dbd41257fde27a6dc4ca11451b7
sugitaku222719 <sugitaku222719@gmail.com>
2024-10-03 16:54:46 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show, :edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,8 +21,21 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
     @item = Item.find(params[:id])
-    
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
@@ -29,4 +44,15 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :content, :category_id, :condition_id, :shipping_fee_id, :area_id, :period_id,
                                  :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
