f984a94ab7b789588740ccf496955a157e9d590d
sugitaku222719 <sugitaku222719@gmail.com>
2024-10-03 14:15:12 +0900

    不要なコメントアウトの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,9 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.order('created_at DESC')
   end
-  
+
   def new
     @item = Item.new
   end
