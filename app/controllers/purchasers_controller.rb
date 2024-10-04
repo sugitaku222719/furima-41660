ac5b6761daa25e4b2ee9c0c7c177e0474ad0a86c
sugitaku222719 <sugitaku222719@gmail.com>
2024-10-04 16:44:17 +0900

    ルーティング、コントローラーへの記述、viewへの記述(クレカ以外)

--- app/controllers/purchasers_controller.rb
+++ app/controllers/purchasers_controller.rb
@@ -1,2 +1,32 @@
 class PurchasersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+  before_action :set_item, only: [:index, :create]
+
+  def index
+  end
+
+  def new
+    @purchaser_history = PurchaserHistory.new
+  end
+
+  def create
+    @purchaser_history = PurchaserHistory.new(purchaser_params)
+    if @purchaser_history.valid?
+      @purchaser_history.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def purchaser_params
+    params.require(:purchaser_history).permit(:postal_code, :area_id, :city, :address_line1, :address_line2, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
